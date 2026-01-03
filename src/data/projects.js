ba7f1d43d83b5630be8e377383f89a9a8ccdf38b
Krishna Gupta <krishna.gupta2023a@vitstudent.ac.in>
2026-01-04 02:17:17 +0530

    png to jpg change made

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,6 +1,6 @@
-import project1 from "../assets/images/project1.png";
-import project2 from "../assets/images/project2.png";
-import project3 from "../assets/images/project3.png";
+import project1 from "../assets/images/project1.jpg";
+import project2 from "../assets/images/project2.jpg";
+import project3 from "../assets/images/project3.jpg";
 
 const projects = [
   {
