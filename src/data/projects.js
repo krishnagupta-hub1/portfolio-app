5e5899755570515dd336924b54753fadd10e2afc
Krishna Gupta <krishna.gupta2023a@vitstudent.ac.in>
2026-01-04 02:20:00 +0530

    changes

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,6 +1,6 @@
 import project1 from "../assets/images/project1.jpg";
 import project2 from "../assets/images/project2.jpg";
-import project3 from "../assets/images/project3.jpg";
+//import project3 from "../assets/images/project3.jpg";
 
 const projects = [
   {
