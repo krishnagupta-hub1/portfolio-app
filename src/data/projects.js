32ae30763da5b84f2a225b030242f48cf5e9fc70
Krishna Gupta <krishna.gupta2023a@vitstudent.ac.in>
2026-01-04 02:11:52 +0530

    Fixing the image issue in vercel

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,23 +1,27 @@
+import project1 from "../assets/images/project1.png";
+import project2 from "../assets/images/project2.png";
+import project3 from "../assets/images/project3.png";
+
 const projects = [
   {
     id: 1,
     title: "IRCTC Clone",
     description: "Train search and booking UI built with React.",
-    image: "/src/assets/images/project1.png",
+    image: project1,
     liveUrl: "https://example-irctc-live-app.com"
   },
   {
     id: 2,
     title: "Portfolio Website",
     description: "Personal portfolio built using React and Vite.",
-    image: "/src/assets/images/project2.png",
+    image: project2,
     liveUrl: "https://example-portfolio-live.com"
   },
   {
     id: 3,
     title: "Todo App",
     description: "Task management app with clean UI.",
-    image: "/src/assets/images/project3.png",
+    image: project3,
     liveUrl: "https://example-todo-live.com"
   }
 ];
